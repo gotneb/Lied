7e2d4d0076129e48e3802450597b6cbffa8251c3
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2025-03-08 23:32:03 -0300

    fix: search bar doesn't have ime action to search

--- app/src/main/java/com/gotneb/lied/music_player/presentation/music_list/components/SearchBar.kt
+++ app/src/main/java/com/gotneb/lied/music_player/presentation/music_list/components/SearchBar.kt
@@ -3,6 +3,8 @@ package com.gotneb.lied.music_player.presentation.music_list.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -14,8 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.gotneb.lied.ui.theme.LiedTheme
+import kotlin.math.max
 
 @Composable
 fun SearchBar(
@@ -24,6 +29,7 @@ fun SearchBar(
     onSearchClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     TextField(
         value = value,
         onValueChange = onValueChange,
@@ -32,6 +38,7 @@ fun SearchBar(
                 text = "Search for a music...",
             )
         },
+        singleLine = true,
         maxLines = 1,
         trailingIcon = {
             IconButton(
@@ -43,6 +50,17 @@ fun SearchBar(
                 )
             }
         },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                if (value.isNotBlank()) {
+                    onSearchClick
+                    keyboardController?.hide() // Hide keyboard after action
+                }
+            }
+        ),
         colors = TextFieldDefaults.colors(
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent,
